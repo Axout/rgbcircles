7971c6013b0a24bc58bf38b4894050c891b81e9b
Axout <rako-serge@ya.ru>
2020-09-06 21:51:44 +0300

    Кнопка "Поехали" 1. Добавлена стартовый экран с запускающей кнопкой "Поехали"; 2. Космический фон

--- app/src/main/java/ru/axout/rgbcircles/MainActivity.java
+++ app/src/main/java/ru/axout/rgbcircles/MainActivity.java
@@ -1,19 +1,32 @@
 package ru.axout.rgbcircles;
 
 import android.app.Activity;
+import android.view.View;
 import android.view.Window;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    Button buttonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // отключение заголовка (верхней плашки) у окна
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        // выбор файла с xml-разметкой происходит с помощью метода setContentView(),
-        // где аргументом является константа, показывающая какой файл нужно взять из папки "res"
-        // название константы совпадает с именем файла рез расширения
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // отключение заголовка (верхней плашки) у окна
+
+        setContentView(R.layout.activity_start);
+
+        buttonStart = findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // выбор файла с xml-разметкой происходит с помощью метода setContentView(),
+                // где аргументом является константа, показывающая какой файл нужно взять из папки "res"
+                // название константы совпадает с именем файла рез расширения
+                setContentView(R.layout.activity_main);
+            }
+        });
     }
 }
